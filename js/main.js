29e36547122230f2394795a2e4d1152ee6d2e610
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-11-25 14:54:10 +0100

    prepping navigation and footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+/**
+ * Toggle mewnu for mobile devices
+ */
+const burger = document.querySelector(".burger");
+let navlist = document.querySelector(".nav-list");
+
+let toggleMenu = () => {
+  burger.classList.toggle("open");
+  navlist.classList.toggle("menu-open");
+};
