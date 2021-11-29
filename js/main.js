2ede58a1e4f95e7a2c4c922386362de72a1d3121
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-11-29 15:33:39 +0100

    frontpage work

--- js/main.js
+++ js/main.js
@@ -11,37 +11,4 @@ let toggleMenu = () => {
   navlist.classList.toggle("menu-open");
 };
 
-/* 
-let heroSlides = [
-  {
-    "img-url": "images/hero1.jpg",
-    "p-text": "Jeg er slide nr. 1",
-    "btn-href": "pages/kalender.html",
-  },
-  {
-    "img-url": "../images/fav_icon.svg",
-    "p-text": "Jeg er slide nr. 2",
-    "btn-href": "pages/om-aska.html",
-  },
-  {
-    "img-url": "images/hero1.jpg",
-    "p-text": "Jeg er slide nr. 3",
-    "btn-href": "pages/aska-ungdom.html",
-  },
-];
-
-let section = document.querySelector(".frontpage-hero");
-let pText = document.querySelector(".hero-text");
-let readMore = document.querySelector(".hero-read-more");
-
-let i = 0;
-let timer = setInterval(() => {
-  if (i >= heroSlides.length) {
-    i = 0;
-  }
-  section.style.background = `url(${heroSlides[i]["img-url"]})`;
-  pText.innerHTML = heroSlides[i]["p-text"];
-  readMore.innerHTML = heroSlides[i]["btn-href"];
-  i++;
-}, 5000);
- */
+document.querySelector(".button-section").scrollLeft = 95;
